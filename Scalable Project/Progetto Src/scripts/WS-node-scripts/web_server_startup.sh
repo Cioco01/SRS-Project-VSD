5769525743e2e8426f700c97288b34acf5b54835
Cioco01 <ciocoiu9@gmail.com>
2025-07-15 10:45:24 +0200

    MOdifica file startup dei 2 nodi

--- Scalable Project/Progetto Src/scripts/WS-node-scripts/web_server_startup.sh
+++ Scalable Project/Progetto Src/scripts/WS-node-scripts/web_server_startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y nginx
-echo '<h1>NetChaos Web Server</h1><p>This is a simulated web server.</p>' | sudo tee /var/www/html/index.html
-sudo systemctl start nginx
-sudo systemctl enable nginx
+
 # Abilita SSH 
 sudo sed -i 's/^#\?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart ssh
